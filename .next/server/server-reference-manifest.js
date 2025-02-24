c9ec48f69f49953b9ae0d92c08fbf81bc71ba594
Kaise White <kaisewhite@gmail.com>
2025-02-24 13:32:38 -0500

    init

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"k+9x+NuxVWM2wWI4aTyMf0QSauqGc4KrHhGsc7mhgzs=\"\n}"
--- app/(unauthenticated)/layout.js
+++
@@ -1,3 +0,0 @@
-export default function UnauthenticatedLayout({ children }) {
-  return <div className='min-h-screen flex items-center justify-center'>{children}</div>;
-}
